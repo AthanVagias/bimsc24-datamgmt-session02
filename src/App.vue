4d8fb46055da40de51f660acc028f844fd44b803
AthanVagias <athanasios.vagias@students.iaac.net>
2024-02-01 16:33:09 +0100

    replace website code in AppVue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,110 @@
-<!-- the script is where the js code goes -->
 <script setup>
-import { ref } from "vue" // you need this to use ref()
+import { ref, onMounted } from 'vue'
 
+// reactive state
+const count = ref(0)
+
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
 
 </script>
 
-
-
-<!-- the template is where the html code goes -->
 <template>
+ <div id="navbar" class="container">  
+        <div id="title">David's bootcamp website website</div>
+        <div id="logo">
+            <img src="@/assets/cow.jpg" alt="macad cow">
 
+        </div>
+        
 
+    </div>
 
+    <div id="flex">
+
+        <div id="sidebar" class="container"> Sidebar </div>
+
+        <div id="main" class="container"> Text input </div>
+    </div>
+    <button @click="increment">Count is: {{ count }}</button>
 </template>
 
-
-
-<!-- style is where the css code goes -->
 <style scoped>
+html{
+    
+    background-color: rgb(59, 59, 59); 
+    color: white;
+}
+
+body{
+
+    margin:0;
+}
+
+div{
+
+    /* put the borders in the inside of container */
+    box-sizing: border-box;
+}
+
+img{
+
+    height: 100%;
+    width: auto;
+
+}
+
+#navbar{
+
+    height: 50px;
+    border-color: red;
 
 
+}
+
+#flex{
+    
+    display: flex;
+    height: calc(100vh - 50px);
+}
+
+#sidebar{
+    
+    width:30%;
+    border-color: blue;
+
+}
+
+#main{
+    width:70%;
+    border-color: green;
+
+}
+
+#title{
+    width: 70%;
+    float:left;
+}
+
+#logo{
+    width: 30%;
+    height: 100%;
+    float:right;
+    text-align: right;
+
+}
+
+.container{
+
+    border-style: dotted;
+    border-width: 1px;
+
+}
 </style>
